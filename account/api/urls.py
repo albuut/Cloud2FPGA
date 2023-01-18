6f6e01c8da2a6459ce101beb8af05c38a6efbf3b
JoseC678 <cervantesjose657@gmail.com>
2023-01-17 17:36:17 -0800

    Added temp page to see the data base that we are adding

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path
+
+from django.conf.urls import url, include
+from .views import GameViewSet,UserGameViewSet
+from rest_framework.routers import DefaultRouter
+
 from account.api.views import(
     registration_view,
 )
 
 from rest_framework.authtoken.views import obtain_auth_token
 
+
+router = DefaultRouter()
+router.register("usergameinfo", UserGameViewSet, basename="usergameinfo")
+router.register("gameinfo",GameViewSet, basename="gameinfo")
+
+
 app_name = "account"
 
 urlpatterns = [
+    url('',include(router.urls)),
     path('register', registration_view, name = 'register'),
     path('login', obtain_auth_token, name = 'login')
 ]
+
+
